460a4ff198673b917358fa9f7044a2763278146c
Curious <curious.startpro@gmail.com>
2021-04-01 12:23:11 +0500

    Merged: merged rasulovmuxtor branch into dev_curious

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,12 +1,10 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path,include
-from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('user.urls')),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('api/users/', include('user.urls')),
 ]
 
 if settings.DEBUG:
--- user/views.py
+++ user/views.py
@@ -1,21 +1,80 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.settings import api_settings
-from user.serializers import UserSerializer, AuthTokenSerializer
+from rest_framework import generics, mixins, status, viewsets
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.views import APIView
 
-class CreateUserView(generics.CreateAPIView):
-    serializer_class = UserSerializer
+from user.serializers import (ChangePasswordSerializer, RegisterSerializer,
+                              UserAvatarSerializer, UserInfoSerializer)
 
-class CreateTokenView(ObtainAuthToken):
-    serializer_class = AuthTokenSerializer
-    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+from .permissions import IsOwnerOrReadOnly
 
-class UserList(generics.ListAPIView):
+
+class UsersApiRoot(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        return Response({
+            'Create user (Sign-Up)': reverse('user:create', request=request, format=format),
+            'Create token for user': reverse('user:token_auth', request=request, format=format),
+            "Current user's profile info": reverse('user:current-profile', request=request, format=format),
+            "Current user's profile avatar": reverse('user:current-avatar', request=request, format=format),
+            # "User info": reverse('user:user-info', request=request, format=format),
+            # "Update user avatar": reverse('user:user-avatar', request=request, format=format),
+            "Change password": reverse('user:change-password', request=request, format=format)
+        })
+
+
+class RegisterView(generics.CreateAPIView):
     queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
 
 
-class UserDetail(generics.RetrieveAPIView):
+class UserInfoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UserInfoSerializer
     queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+
+class UserAvatarAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UserAvatarSerializer
+    queryset = get_user_model().objects.all()
+    permission_classes = [IsOwnerOrReadOnly]
+
+
+class CurrentUserAvatarAPIView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserAvatarSerializer
+    queryset = get_user_model().objects.all()
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
+
+
+class CurrentUserInfoView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserInfoSerializer
+    queryset = get_user_model().objects.all()
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
+
+
+class ChangePasswordView(generics.UpdateAPIView):
+    serializer_class = ChangePasswordSerializer
+
+    def update(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        # if using drf authtoken, create a new token
+        if hasattr(user, 'auth_token'):
+            user.auth_token.delete()
+        token, created = Token.objects.get_or_create(user=user)
+        # return new token
+        return Response({'token': token.key}, status=status.HTTP_200_OK)
