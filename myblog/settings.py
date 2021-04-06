171f067589ddc67cd765a13a06640e76cccbb276
Curious <curious.startpro@gmail.com>
2021-04-06 22:11:49 +0500

    Fixed: merge conflicts

--- myblog/settings.py
+++ myblog/settings.py
@@ -4,13 +4,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-<<<<<<< HEAD:myblog/settings.py
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-=======
->>>>>>> origin/rasulovmuxtor:blog/blog/settings.py
 SECRET_KEY = '56%pn5nyem^-!kpl^3wxf2@4h(m=)&4jjt8c_dicd-vo&)k)0%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -18,11 +11,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-<<<<<<< HEAD:myblog/settings.py
-# Application definition
-
-=======
->>>>>>> origin/rasulovmuxtor:blog/blog/settings.py
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -34,7 +22,6 @@ INSTALLED_APPS = [
     # 3rd part libraries
     'rest_framework',
     'rest_framework.authtoken',
-    'tinymce',
 
     # our apps
     'user.apps.UserConfig',
@@ -70,35 +57,21 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'myblog.wsgi.application'
-<<<<<<< HEAD:myblog/settings.py
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-=======
->>>>>>> origin/rasulovmuxtor:blog/blog/settings.py
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
 
-<<<<<<< HEAD:myblog/settings.py
         'PASSWORD': 'curious',
 
         'HOST': 'localhost',
 
         'PORT': '5432',
-    }
-}
 
-# Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
-
-=======
         'NAME': os.path.join(BASE_DIR,'blog.sqlite3',)
     }
 }
 
->>>>>>> origin/rasulovmuxtor:blog/blog/settings.py
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -114,12 +87,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-<<<<<<< HEAD:myblog/settings.py
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
-
-=======
->>>>>>> origin/rasulovmuxtor:blog/blog/settings.py
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -129,10 +96,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = False
-<<<<<<< HEAD:myblog/settings.py
-=======
-
->>>>>>> origin/rasulovmuxtor:blog/blog/settings.py
 
 AUTH_USER_MODEL = 'user.User'
 
@@ -146,18 +109,9 @@ MEDIA_URL = '/media/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-<<<<<<< HEAD:myblog/settings.py
         'rest_framework.authentication.BasicAuthentication',
-=======
         'rest_framework.authentication.SessionAuthentication',
->>>>>>> origin/rasulovmuxtor:blog/blog/settings.py
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DATETIME_FORMAT': "%Y-%m-%d - %H:%M:%S",
 }
-<<<<<<< HEAD:myblog/settings.py
-=======
-
-TINYMCE_JS_URL = os.path.join(STATIC_URL, "path/to/tiny_mce/tiny_mce.js")
-TINYMCE_JS_ROOT = os.path.join(STATIC_ROOT, "path/to/tiny_mce")
->>>>>>> origin/rasulovmuxtor:blog/blog/settings.py
